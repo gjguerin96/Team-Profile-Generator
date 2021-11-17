2b619f54946d974ac8cec6ba9d44132c6da8a4ce
Gabriel Guerin <gjguerin96@gmail.com>
2021-11-16 21:20:39 -0800

    worked on javascript

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const inquirer = require('inquirer')
+// const inquirer = require('inquirer');
+const Employee = require("./lib/Employee")
+
+const employee = new Employee();
+
+
+
+
+// function askForManager() {
+//     inquirer
+//         .prompt([
+//             {
+//                 type:'input',
+//                 name: 'manname',
+//                 message: 'Enter manager name.'
+//             },
+//             {
+//                 type:'input',
+//                 name: 'id',
+//                 message: 'Enter manager id number.'
+//             },
+//             {
+//                 type:'input',
+//                 name: 'email',
+//                 message: 'Enter manager email address.'
+//             },
+//             {
+//                 type:'input',
+//                 name: 'office',
+//                 message: 'Enter manager office number.'
+//             }
+//         ]);
+//         then(() => {
+//             // const manager = new Manager(manname.value,id.value,email.value,office.value);
+//             console.log(manname.value)
+//         });
+// }
+
+// askForManager();
