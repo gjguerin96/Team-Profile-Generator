8570651e857d3c1bdb3f04f4a8bb931bf74e9756
Gabriel Guerin <gjguerin96@gmail.com>
2021-11-17 17:32:17 -0800

    improved class functionality

--- index.js
+++ index.js
@@ -1,39 +1,103 @@
-// const inquirer = require('inquirer');
+const inquirer = require('inquirer');
 const Employee = require("./lib/Employee")
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
 
-const employee = new Employee();
+// const employee = new Employee();
+// const manager = new Manager();
 
 
+const basicQuestions = [
+    {
+        type:'input',
+        name: 'empname',
+        message: 'Enter name.'
+    },
+    {
+        type:'input',
+        name: 'id',
+        message: 'Enter id number.'
+    },
+    {
+        type:'input',
+        name: 'email',
+        message: 'Enter email address.'
+    },
+]
 
+function askForEngineer() {
+    inquirer
+        .prompt([
+            basicQuestions[0],
+            basicQuestions[1],
+            basicQuestions[2],
+            {
+                type:'input',
+                name: 'github',
+                message: 'Enter github url.'
+            }
+        ])
+        .then((answers) => {
+            const engineer = new Engineer(answers.empname,answers.id,answers.email,answers.github);
+            engineer.getName()
+            engineer.getId()
+            engineer.getEmail()
+            engineer.getGithub()
+            engineer.getRole()
+            whatNext()
+        });
+    
+}
 
-// function askForManager() {
-//     inquirer
-//         .prompt([
-//             {
-//                 type:'input',
-//                 name: 'manname',
-//                 message: 'Enter manager name.'
-//             },
-//             {
-//                 type:'input',
-//                 name: 'id',
-//                 message: 'Enter manager id number.'
-//             },
-//             {
-//                 type:'input',
-//                 name: 'email',
-//                 message: 'Enter manager email address.'
-//             },
-//             {
-//                 type:'input',
-//                 name: 'office',
-//                 message: 'Enter manager office number.'
-//             }
-//         ]);
-//         then(() => {
-//             // const manager = new Manager(manname.value,id.value,email.value,office.value);
-//             console.log(manname.value)
-//         });
-// }
+function whatNext() {
+    inquirer
+        .prompt([
+            {
+                type:'list',
+                name: 'next',
+                message: 'What do you want to do next?',
+                choices: ['Add an engineer','Add an intern','Finish building my team']
+            }  
+        ])
+        .then((answer) => {
+            if (answer.next === 'Add an engineer'){
+                askForEngineer()
+            }
+            else if (answer.next === 'Add an intern'){
+                askForIntern()
+            }
+            else {
+                console.log('still working on this one')
+            }
+        });
+}
 
-// askForManager();
+function askForManager() {
+    inquirer
+        .prompt([
+            {
+                type:'input',
+                name: 'empname',
+                message: 'Enter manager name.'
+            },
+            basicQuestions[1],
+            basicQuestions[2],
+            {
+                type:'input',
+                name: 'office',
+                message: 'Enter manager office number.'
+            }
+        ])
+        .then((answers) => {
+            const mang = new Manager(answers.empname,answers.id,answers.email,answers.office);
+            mang.getName()
+            mang.getId()
+            mang.getEmail()
+            mang.getOffice()
+            mang.getRole()
+            whatNext()
+        });
+    
+}
+
+askForManager();
